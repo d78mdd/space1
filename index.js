ef03b385de854cdd217181b8ca06609e980cea42
Diman Kostadinov <d78mdd@gmail.com>
2021-01-18 14:27:48 +0000

    list and slider

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express app!')
+  res.sendFile(__dirname + "/output.html")
 });
+
+
+app.get('/urlMercury', (req, res) => {
+  res.send("mercury")
+});
+
+
+
 
 app.listen(3000, () => {
   console.log('server started');
